ec817cad449a66d3e322296668faed1455fdf4fc
Леонов Денис <deniss46leonov@gmail.com>
2021-12-21 19:38:28 +0300

    Добавьте файлы проекта.

---
+++ CollectionViewTutorial/CollectionViewTutorial/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Monkey
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string ImageUrl { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
